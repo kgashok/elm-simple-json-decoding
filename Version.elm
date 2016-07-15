cf40734cbfa357634d3cd4ba9c739c70414c9418
kgashok <lifebalance@gmail.com>
2016-07-16 00:05:42 +0530

    bumped up the version for final commit for resolving #35

--- Version.elm
+++ Version.elm
@@ -3,7 +3,7 @@ module Version exposing (..) -- where
 
 
 version : String 
-version = "v3.0-beta-60-gb382917"
+version = "v3.0-beta-63-g77558c7"
 
 gitRepo : String 
 gitRepo = "https://github.com/kgashok/elm-simple-json-decoding"
