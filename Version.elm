150f944e44601a2146615137a65a77285e75933a
kgashok <lifebalance@gmail.com>
2017-03-01 23:27:28 +0530

    Removed the Debug compile option

--- Version.elm
+++ Version.elm
@@ -3,7 +3,7 @@ module Version exposing (..) -- where
 
 
 version : String 
-version = "v3.0-beta-118-gbd78841"
+version = "v3.0-beta-119-g6946ba9"
 
 gitRepo : String 
 gitRepo = "https://github.com/kgashok/elm-simple-json-decoding"
