bd71e55fb0dbb89f57a0746d660eb4d646616d49
kgashok <lifebalance@gmail.com>
2018-01-21 00:18:23 +0000

    CORS

--- dist/elm.js
+++ dist/elm.js
@@ -10659,7 +10659,7 @@ var _user$project$Model$gUserUrl = F3(
 	});
 var _user$project$Model$gitterKey = 'ae28f23f134c4364ad45e7b7355cfa91c92038bb';
 var _user$project$Model$gUrl = A2(_elm_lang$core$Basics_ops['++'], 'https://api.gitter.im/v1/rooms?access_token=', _user$project$Model$gitterKey);
-var _user$project$Model$fccAPI = 'http://www.freecodecamp.org/api/users/about?username=';
+var _user$project$Model$fccAPI = 'https://cors.now.sh/https://www.freecodecamp.org/api/users/about?username=';
 var _user$project$Model$initialModel = {
 	url: _user$project$Model$fccAPI,
 	name: '',
