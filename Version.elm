ddfb20d8b8b3f07fdf646c4cc15ec4da71188085
kgashok <lifebalance@gmail.com>
2018-01-21 00:23:17 +0000

    bumped up the version

--- Version.elm
+++ Version.elm
@@ -3,7 +3,7 @@ module Version exposing (..) -- where
 
 
 version : String 
-version = "v3.0-beta-134-gbd71e55"
+version = "v3.0-beta-135-g65db3e5"
 
 gitRepo : String 
 gitRepo = "https://github.com/kgashok/elm-simple-json-decoding"
